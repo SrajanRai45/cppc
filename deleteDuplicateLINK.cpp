b8fb0cc6b9b46be8b4fd15e080da07ed223e20a6
CreaZone <112389449+SrajanRai45@users.noreply.github.com>
2024-10-27 21:22:25 +0530

    gg

--- deleteDuplicateLINK.cpp
+++ deleteDuplicateLINK.cpp
@@ -14,7 +14,7 @@ public:
         this->next = nullptr;
     }
 };
-
+/*
 ListNode *deleteDuplicates(ListNode *head)
 {
     if (head == nullptr)
@@ -37,4 +37,26 @@ ListNode *deleteDuplicates(ListNode *head)
         }
     }
     return head;
+}*/
+//hand written code above
+
+ListNode *deleteDuplicates(ListNode *head) {
+    if (head == nullptr) {
+        return nullptr; // Return null if the list is empty
+    }
+
+    ListNode *current = head; // Pointer to iterate through the list
+
+    while (current != nullptr && current->next != nullptr) {
+        // Check if the current node's value is the same as the next node's value
+        if (current->val == current->next->val) {
+            ListNode *duplicate = current->next; // Node to be deleted
+            current->next = current->next->next; // Bypass the duplicate
+            delete duplicate; // Free the memory of the duplicate node
+        } else {
+            current = current->next; // Move to the next node
+        }
+    }
+
+    return head; // Return the modified list
 }
